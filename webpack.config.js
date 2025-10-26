ebccf55e5d77b910565b038ff9cb2f2e90c9291e
Максим Мухомедьяров <madmaxim22@yandex.ru>
2025-10-26 16:49:07 +0300

    init: whack-a-goblin project

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default (env) => {
+  const isDev = env.mode === "development";
+  const isProd = env.mode === "production";
+
+  return {
+    mode: env.mode ?? "development",
+    entry: path.resolve(__dirname, "src", "index.js"),
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "[name].[contenthash].js",
+      assetModuleFilename: "images/[hash][ext][query]",
+      clean: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src", "index.html"),
+      }),
+      isProd &&
+        new MiniCssExtractPlugin({
+          filename: "css/[name].[contenthash:8].css",
+          chunkFilename: "css/[name].[contenthash:8].css",
+        }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          resolve: {
+            fullySpecified: false,
+          },
+          use: {
+            loader: "babel-loader",
+            options: { presets: ["@babel/preset-env"] },
+          },
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            isProd ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+    devServer: {
+      port: 5000,
+      open: true,
+    },
+  };
+};
